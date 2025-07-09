05b7c8515557b8f4a0f897ded7d29a9e2baa0bab
Snukala <148600295+saanvin22@users.noreply.github.com>
2025-07-08 20:53:58 -0700

    Add files via upload

---
+++ Production/main.py
@@ -0,0 +1,32 @@
+# You import all the IOs of your board
+import board
+
+# These are imports from the kmk library
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+
+# This is the main instance of your keyboard
+keyboard = KMKKeyboard()
+
+# Add the macro extension
+macros = Macros()
+keyboard.modules.append(macros)
+
+# Define your pins here!
+PINS = [board.D5, board.D6, board.D7, board.D8, board.D9]
+
+# Tell kmk we are not using a key matrix
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+keyboard.keymap = [
+    [KC.Y, KC.KP_ASTERISK, KC.F, KC.M, KC.I]
+]
+
+# Start kmk!
+if __name__ == '__main__':
+    keyboard.go()
